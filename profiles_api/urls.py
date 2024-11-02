7754bf3c9439b0aea1452f47ed70e71aad87465c
salahoddin <salahoddin.sambutuan89@gmail.com>
2024-11-02 17:40:47 +0800

    created api views, persmissions and implemented authentication

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet) # no need to pass base_name because this has a queryset
 urlpatterns = [
     path('hello-view/', views.HelloAPIView.as_view()),
     path('', include(router.urls)),
